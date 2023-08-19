d6ba8d2a338a3fc4f3bb6a85e84aab42ad7370a9
JG-Stadler <joaogbstadler@gmail.com>
2023-08-19 10:41:30 -0300

    Revert "tentativa falha"
    This reverts commit f7641803ae63c856157f440df42329fa031a201f.

--- js/script.js
+++ js/script.js
@@ -117,12 +117,6 @@ alert("Esta função não está disponivel no momento.");
 {
     var buscar = document.getElementById("buscar");
 
-            // for(var i=0;i<=prods.length;i++){
-            //         if(input === " "){
-            //             prods[i].style.display = ""
-            //         }
-            }
-
         buscar.addEventListener("click",()=>{
             var input = document.getElementById("input");
                 input = input.value.toLowerCase();
